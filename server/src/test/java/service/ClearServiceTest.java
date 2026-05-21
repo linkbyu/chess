5a6c5143bc26b4e9e7f4a4118bdea2545873dab3
Ben Thompson <linkbyu@student.byu.edu>
2026-05-20 23:56:46 -0600

    (p3) all passoff tests passed; Unit Tests Added

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,15 +1,51 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.GameDAO;
-import dataaccess.UserDAO;
+import chess.ChessGame;
+import dataaccess.*;
+import dataaccess.exception.BadRequestException;
+import dataaccess.exception.DataAccessException;
+import dataaccess.exception.UnauthorizedException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.params.CreateRequest;
+import service.params.JoinRequest;
+import service.params.LoginRequest;
+import service.params.RegisterRequest;
 
 public class ClearServiceTest {
-    private UserDAO userDAO;
-    private GameDAO gameDAO;
-    private AuthDAO authDAO;
+
+    private UserService userService;
+    private GameService gameService;
+    private ClearService clearService;
 
 
+    @BeforeEach
+    void setUp(){
+        UserDAO userDAO = new MemoryUserDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
 
+        userService = new UserService(userDAO, authDAO);
+        gameService = new GameService(userDAO, gameDAO);
+        clearService = new ClearService(userDAO, gameDAO, authDAO);
+    }
+
+    @Test
+    void clearAllData() throws DataAccessException, BadRequestException {
+        userService.register(new RegisterRequest("bob", "hehe", null));
+        userService.register(new RegisterRequest("tom", "abc", null));
+        int gameID = gameService.createGame(new CreateRequest("duelOfTheFates"));
+        gameService.joinGame(new JoinRequest(ChessGame.TeamColor.WHITE, gameID), "tom");
+        gameService.joinGame(new JoinRequest(ChessGame.TeamColor.BLACK, gameID), "bob");
+
+
+        clearService.clear();
+        Assertions.assertThrows(UnauthorizedException.class, () ->
+                userService.login(new LoginRequest("bob", "hehe")));
+        Assertions.assertThrows(UnauthorizedException.class, () ->
+                userService.login(new LoginRequest("tom", "abc")));
+        Assertions.assertTrue( gameService.listGames().isEmpty() );
+    }
 
 }
